2a8542101c3e54baa4454c8e9810d3431b70e81f
shray <sh290173@dal.ca>
2025-06-20 21:56:09 -0300

    login form

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import { productReducer } from './productReducer';
 import { authReducer    } from './authReducer';
